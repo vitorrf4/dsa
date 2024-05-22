c6c2092786b1477c6eb710b3a7b907db014d04a3
vitor <vitorrf4@gmail.com>
2024-05-22 18:38:17 -0300

    improve queue class

--- src/datastructures/Queue.java
+++ src/datastructures/Queue.java
@@ -1,19 +1,20 @@
 package datastructures;
 
-public class Queue {
-    private final Integer[] items;
+public class Queue<T> {
+    private final T[] items;
     private int front;
     private int rear;
 
     public Queue(int size) {
-        items = new Integer[size];
+        items = (T[]) new Object[size];
         front = -1;
         rear = -1;
     }
 
-    public boolean enqueue(int newItem) {
+    // Inserts the specified element into this queue
+    public boolean enqueue(T newItem) {
         if (isFull())
-            return false;
+            throw new IllegalStateException("Queue is full");
 
         items[++rear] = newItem;
         if (rear == 0) front = 0;
@@ -21,12 +22,13 @@ public class Queue {
         return true;
     }
 
-    public Integer dequeue() {
+    // Retrieves and removes the head of this queue
+    public T dequeue() {
         if (isEmpty())
-            return null;
+            throw new IllegalStateException("Queue is empty");
 
         if (front >= rear) {
-            int item = items[front];
+            T item = items[front];
             front = -1;
             rear = -1;
             return item;
@@ -35,18 +37,26 @@ public class Queue {
         return items[front++];
     }
 
+    // Tests if this queue is empty
     public boolean isEmpty() {
         return front == -1;
     }
 
+    // Tests if this queue is full
     public boolean isFull() {
         return rear == items.length - 1;
     }
 
-    public Integer peek() {
+    // Retrieves, but does not remove, the head of this queue
+    public T peek() {
         if (isEmpty())
-            return null;
+            throw new IllegalStateException("Queue is empty");
 
         return items[front];
+    }
+
+    // Returns the number of elements in this queue
+    public int size() {
+        return rear - front + 1;
     }
 }
--- src/datastructures/Stack.java
+++ src/datastructures/Stack.java
@@ -1,45 +1,55 @@
 package datastructures;
 
-public class Stack {
-    private final Integer[] items;
+public class Stack<T> {
+    private final T[] items;
     private int top;
 
     public Stack(int size) {
-        items = new Integer[size];
+        items = (T[]) new Object[size];
         top = -1;
     }
 
-    public boolean push(int newItem) {
+    // Pushes an item onto the top of this stack
+    public boolean push(T newItem) {
         if (isFull())
-            return false;
+            throw new IllegalStateException("Stack is full");
 
         items[++top] = newItem;
 
         return true;
     }
 
-    public Integer pop() {
+    // Removes the object at the top of this stack and returns that object
+    public T pop() {
         if (isEmpty())
-            return null;
+            throw new IllegalStateException("Stack is empty");
 
-        int item = items[top];
+        T item = items[top];
         items[top--] = null;
 
         return item;
     }
 
+    // Tests if this stack is empty
     public boolean isEmpty() {
         return top == -1;
     }
 
+    // Tests if this stack is full
     public boolean isFull() {
         return top == items.length - 1;
     }
 
-    public Integer peek() {
+    // Looks at the object at the top of this stack without removing it from the stack
+    public T peek() {
         if (isEmpty())
-            return null;
+            throw new IllegalStateException("Stack is empty");
 
         return items[top];
+    }
+
+    // Returns the number of elements in this stack
+    public int size() {
+        return top + 1;
     }
 }
